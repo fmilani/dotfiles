76ed7b79eea9a19a9adb4a96bfb51a021db3a3a9
Felipe Milani <felmilani@gmail.com>
2023-10-27 22:57:14 -0300

    add vim-tmux-navigator

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -215,6 +215,8 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
+  'christoomey/vim-tmux-navigator',
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
